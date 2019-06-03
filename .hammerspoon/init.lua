30e3be00aab92194a6d32436eda043a4caf7a7ad
crabdul <23074754+crabdul@users.noreply.github.com>
2019-06-03 10:53:57 +0100

    Revert "Update init lua file"
    This reverts commit eecaf1096eb83ab7d93c933b5f5868ea4043edc7.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,1 +1,13 @@
-/Users/abdulkarim/.dotfiles/.hammerspoon/init.lua
+-- Setup --
+local mash = {"cmd", "alt"}
+
+-- Global keyboard shortcuts --
+hs.hotkey.bind(mash, 'H', function() hs.application.launchOrFocus('Google Chrome') end)
+hs.hotkey.bind(mash, 'K', function() hs.application.launchOrFocus('iTerm') end)
+hs.hotkey.bind(mash, 'P', function() hs.application.launchOrFocus('Spotify') end)
+hs.hotkey.bind(mash, 'U', function() hs.application.launchOrFocus('Firefox') end)
+hs.hotkey.bind(mash, 'J', function() hs.application.launchOrFocus('Visual Studio Code') end)
+hs.hotkey.bind(mash, 'O', function() hs.application.launchOrFocus('Slack') end)
+hs.hotkey.bind(mash, 'N', function() hs.application.launchOrFocus('Notion') end)
+hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Mail') end)
+
