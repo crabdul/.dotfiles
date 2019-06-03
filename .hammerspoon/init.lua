9119409e90d202c521db86c3e17992bca8824c4e
crabdul <23074754+crabdul@users.noreply.github.com>
2019-06-03 11:20:49 +0100

    Update hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,6 +3,8 @@ local mash = {"cmd", "alt"}
 
 -- Global keyboard shortcuts --
 hs.hotkey.bind(mash, 'J', function() hs.application.launchOrFocus('Google Chrome') end)
+hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Safari') end)
 hs.hotkey.bind(mash, 'K', function() hs.application.launchOrFocus('iTerm') end)
 hs.hotkey.bind(mash, 'P', function() hs.application.launchOrFocus('Spotify') end)
 hs.hotkey.bind(mash, 'L', function() hs.application.launchOrFocus('Slack') end)
+hs.hotkey.bind(mash, 'H', function() hs.application.launchOrFocus('TickTick') end)
