025ff7520eeccce4455ab26a0dd43dd44ca60608
crabdul <23074754+crabdul@users.noreply.github.com>
2019-12-14 11:57:53 +0000

    Add preview hotkey

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,7 +3,7 @@ local mash = {"cmd", "alt"}
 
 -- Global keyboard shortcuts --
 hs.hotkey.bind(mash, 'J', function() hs.application.launchOrFocus('Google Chrome') end)
-hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Safari') end)
+hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Preview') end)
 hs.hotkey.bind(mash, 'K', function() hs.application.launchOrFocus('iTerm') end)
 hs.hotkey.bind(mash, 'P', function() hs.application.launchOrFocus('Spotify') end)
 hs.hotkey.bind(mash, 'L', function() hs.application.launchOrFocus('Slack') end)
