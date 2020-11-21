b5afea2ba7a4f39123893e325702a637ad949447
crabdul <23074754+crabdul@users.noreply.github.com>
2020-11-21 10:00:37 +0000

    Update commands for hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,11 +3,11 @@ local mash = {"cmd", "alt"}
 
 -- Global keyboard shortcuts --
 hs.hotkey.bind(mash, 'J', function() hs.application.launchOrFocus('Google Chrome') end)
-hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Preview') end)
+hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Insomnia') end)
 hs.hotkey.bind(mash, 'K', function() hs.application.launchOrFocus('iTerm') end)
 hs.hotkey.bind(mash, 'P', function() hs.application.launchOrFocus('Postico') end)
 hs.hotkey.bind(mash, 'L', function() hs.application.launchOrFocus('Slack') end)
-hs.hotkey.bind(mash, 'H', function() hs.application.launchOrFocus('Safari') end)
+hs.hotkey.bind(mash, 'W', function() hs.application.launchOrFocus('Safari') end)
 hs.hotkey.bind(mash, 'U', function() hs.application.launchOrFocus('Quiver') end)
 hs.hotkey.bind(mash, 'N', function() hs.application.launchOrFocus('SnippetsLab') end)
-hs.hotkey.bind(mash, ',', function() hs.application.launchOrFocus('Dash') end)
+hs.hotkey.bind(mash, 'Y', function() hs.application.launchOrFocus('Visual Studio Code') end)
