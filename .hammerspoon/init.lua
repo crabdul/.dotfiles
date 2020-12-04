6db25bb8ceb8e5f81a95c26689b2835915c47ffd
crabdul <23074754+crabdul@users.noreply.github.com>
2020-12-04 19:32:29 +0000

    Update hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,11 +3,12 @@ local mash = {"cmd", "alt"}
 
 -- Global keyboard shortcuts --
 hs.hotkey.bind(mash, 'J', function() hs.application.launchOrFocus('Google Chrome') end)
-hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Insomnia') end)
+hs.hotkey.bind(mash, 'G', function() hs.application.launchOrFocus('Google Chrome Canary') end)
+hs.hotkey.bind(mash, 'N', function() hs.application.launchOrFocus('Insomnia') end)
 hs.hotkey.bind(mash, 'K', function() hs.application.launchOrFocus('iTerm') end)
 hs.hotkey.bind(mash, 'P', function() hs.application.launchOrFocus('Postico') end)
-hs.hotkey.bind(mash, 'L', function() hs.application.launchOrFocus('Slack') end)
-hs.hotkey.bind(mash, 'W', function() hs.application.launchOrFocus('Safari') end)
-hs.hotkey.bind(mash, 'U', function() hs.application.launchOrFocus('Quiver') end)
-hs.hotkey.bind(mash, 'N', function() hs.application.launchOrFocus('SnippetsLab') end)
-hs.hotkey.bind(mash, 'Y', function() hs.application.launchOrFocus('Visual Studio Code') end)
+hs.hotkey.bind(mash, ';', function() hs.application.launchOrFocus('Slack') end)
+hs.hotkey.bind(mash, 'A', function() hs.application.launchOrFocus('Safari') end)
+hs.hotkey.bind(mash, 'F', function() hs.application.launchOrFocus('Quiver') end)
+hs.hotkey.bind(mash, 'S', function() hs.application.launchOrFocus('SnippetsLab') end)
+hs.hotkey.bind(mash, 'D', function() hs.application.launchOrFocus('Visual Studio Code') end)
